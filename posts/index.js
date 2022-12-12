0808876c318e37a0214d6753d66c5abc4f13df81
VividTruthKeeper <mr.aydogdy02@gmail.com>
2022-12-12 21:20:09 +0500

    skaffold

--- posts/index.js
+++ posts/index.js
@@ -14,7 +14,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -40,6 +40,7 @@ app.post("/events", (req, res) => {
   res.send({});
 });
 
-app.listen(4000, () => {
-  console.log("Listening on 4000");
+app.listen(5000, () => {
+  console.log("v1000");
+  console.log("Listening on 5000");
 });
